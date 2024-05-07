10df4bdd07d142ef598b1e0c2f1a9279c2321d36
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2024-05-07 15:51:27 -0400

    commit

--- index.py
+++ index.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
+@app.route('/home')
+def base():
+    return render_template('base.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
