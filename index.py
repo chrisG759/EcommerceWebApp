abf7f92ec40e8c3a8412c277c91eaa862e23048a
nharalam32 <nharalam32@gmail.com>
2024-05-08 14:23:47 -0400

    changes

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-from flask import Flask, redirect, url_for, render_template
+from datetime import  timedelta
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import generate_csrf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
@@ -23,10 +26,6 @@ class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-
-app.config['SECRET_KEY'] = 'key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://chris:sirhc@172.16.181.82/fp180'
-db = SQLAlchemy(app)
 
 class Product(db.Model):
     __tablename__ = 'products'
@@ -56,16 +55,63 @@ class Cart(db.Model):
 def home():
     return render_template('base.html')
 
-@app.route('/home')
-def base():
-    return render_template('base.html')
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user.password == password:
+            session['user_id'] = user.id
+            session.permanent = True
+            return redirect(url_for('products'))
+        else:
+            flash('Invalid username or password', 'error')
+    
     return render_template('login.html')
 
-@app.route('/register')
-def register ():
+@app.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        admin = Admin.query.filter_by(username=username, password=password).first()
+        
+        if admin:
+            session['admin_id'] = admin.id
+            return redirect(url_for('admin'))
+        else:
+            flash('Invalid username or password', 'error')
+    
+    return render_template('admin_login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        
+        new_user = User(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        session['user_id'] = new_user.id
+        
+        return redirect(url_for('products'))
+    
     return render_template('register.html')
 
 @app.route('/products')
